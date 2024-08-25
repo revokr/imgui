c42492e639191495584d66e28720e0e2dbbdf0ab
revokr <101871749+revokr@users.noreply.github.com>
2024-08-25 23:39:23 +0300

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+project "IMGUI"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	warnings "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imgui_demo.cpp",
+        "imgui_tables.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h"
+
+	}
+
+	defines 
+		{
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
